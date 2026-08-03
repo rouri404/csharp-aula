65b766913f8a14a1c6427220b1cbe97421b21718
Gabriel Couto <gabriel.couto2704@gmail.com>
2026-08-03 11:18:40 -0300

    feat: aula1

---
+++ MeuPrimeiroApp/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Olá mundo");
+
+        // Nome
+        Console.Write("Qual seu nome? ");
+        string nome = Console.ReadLine();
+
+        // Idade
+        Console.Write("Qual sua idade? ");
+        string idade = Console.ReadLine();
+
+        // Curso
+        Console.Write("Qual seu curso? ");
+        string curso = Console.ReadLine();
+
+        Console.WriteLine($"Meu nome é {nome}, tenho {idade} anos de idade, e estou cursando {curso}");
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Aula 1 - 03/08/2026
+
+Olá mundo.
+
+---
+
+[🠔 Mais aulas](https://github.com/rouri404/csharp-aula)
