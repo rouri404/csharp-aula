0fff56a0e566ff3d5e45b5ccd8e96991465e5506
Gabriel Couto <gabriel.couto2704@gmail.com>
2026-08-17 11:11:52 -0300

    feat: aula3

---
+++ Aula3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Aula3 {
+    public class Aula3 {
+        static void Main() {
+            Console.WriteLine("Aula 3 - 17/08");
+
+            Console.WriteLine("1 - Digite um número: ");
+            string input = Console.ReadLine();
+            int number1;
+
+            if (int.TryParse(input, out number1))
+            {
+                
+            } else
+            {
+                
+            }
+            ;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Aula 3 - 17/08/2026
+
+sla porra
+
+---
+
+[🠔 Mais aulas](https://github.com/rouri404/csharp-aula)
